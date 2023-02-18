80c094f5442401d13d5a8eefc2936d3b903eb882
webjunjun <meliujun@qq.com>
2023-02-18 17:21:44 +0800

    feat: 去掉配置密码等信息

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import { NestSeedType } from 'src/type/nestSeed';
+
+// 程序常用变量
+export const NestSeedConstants: NestSeedType = {
+  secret: 'xxx',
+  appid: 'xxx',
+  appsecret: 'xxx',
+  wechatHost: 'https://api.weixin.qq.com/'
+};
